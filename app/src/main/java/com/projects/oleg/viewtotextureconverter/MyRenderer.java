6b10842d51fed8fadbaa9060ca3c29e04dda4338
oleg@laptop <o.tolstov1569@gmail.com>
2015-07-06 00:50:07 -0700

    added culling, rearanged scene

--- app/src/main/java/com/projects/oleg/viewtotextureconverter/MyRenderer.java
+++ app/src/main/java/com/projects/oleg/viewtotextureconverter/MyRenderer.java
@@ -43,6 +43,7 @@ public class MyRenderer implements CardboardView.StereoRenderer {
     private Plane tstPlane = new Plane();
     private volatile View[] content;
     private volatile VirtualDisplayPlane[] contentPlanes;
+    private float radius;
 
     public MyRenderer(Context context, View[] content){
         super();
@@ -56,7 +57,8 @@ public class MyRenderer implements CardboardView.StereoRenderer {
     }
 
     public void positionPlanes(float rad) {
-        float mag = .5f;
+        radius = rad;
+        float mag = .9f;
         float angle;
         if(contentPlanes.length == 1){
             float dx = (float) (rad*Math.cos(Math.toRadians(90)));
@@ -67,7 +69,7 @@ public class MyRenderer implements CardboardView.StereoRenderer {
             return;
         }
         angle = 180 / (contentPlanes.length - 1);
-        float distance =8f;
+        float distance = 6f;
         float startX = distance*(contentPlanes.length - 1)/2.0f;
         for (int i = 0; i < contentPlanes.length; i++) {
             float dxL =  -startX + distance*i;
@@ -76,13 +78,16 @@ public class MyRenderer implements CardboardView.StereoRenderer {
             float dz = (float) (rad*Math.sin(Math.toRadians(angle * i)));
             float mx = dx + (dxL - dx)*mag;
             float mz = dz + (dzL - dz)*mag;
+            contentPlanes[i].displace(mx, 0, mz);
+            float[] axis = {0,1,0,0};
+            float rotate = -15;
 
-            contentPlanes[i].displace(dxL, 0, dzL);
-            //contentPlanes[i].lookAt(camera.getOrigin(), camera.getDown());
+            //contentPlanes[i].rotateAboutPoint(axis,rotate - rotate*i,contentPlanes[i].getOrigin());
+            contentPlanes[i].lookAt(camera.getOrigin(),camera.getDown());
             Utils.print("Plane Origin " + i);
             Utils.printVec(contentPlanes[i].getOrigin());
             contentPlanes[i].scale(16.0f, 9.0f, 1);
-            contentPlanes[i].scale(1.0f / 3.7f, 1.0f / 3.7f, 1);
+            contentPlanes[i].scale(1.0f /6.3f, 1.0f / 6.3f, 1);
             Utils.print("Plane bounds are");
             Utils.printMat(contentPlanes[i].getBounds());
         }
@@ -99,20 +104,23 @@ public class MyRenderer implements CardboardView.StereoRenderer {
     @Override
     public void onDrawEye(Eye eye) {
         camera.setRatio((float) eye.getViewport().height / (float) eye.getViewport().width);
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+        GLES20.glDepthFunc(GLES20.GL_LEQUAL);
         GLES20.glClearColor(1, 0, 0, 0);
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT);
         eyeCamera.copyFrom(camera);
         eyeCamera.applyTransform(eye.getEyeView());
-        //eyeCamera.setFov(eye.getFov().getLeft() + eye.getFov().getRight());
+       // eyeCamera.setFov(eye.getFov().getLeft() + eye.getFov().getRight());
         float[] crossProduct = new float[4];
         for(int i = 0; i < contentPlanes.length; i++){
             float[] bounds = contentPlanes[i].getBounds();
             boolean skip = false;
-            for(int b = 0; b < 4; b++){
-                float dotProduct = Utils.dotProduct(bounds,4*b,eyeCamera.getForward(),0);
-                if( dotProduct <= 0){
+            for(int b = 0; b < 4 ; b++){
+                float dotProduct = Utils.dotProduct(bounds,b*4,eyeCamera.getForward(),0) / Utils.getMagnitude(bounds,b*4);
+                Utils.print("Plane "+ i + "Dot product is " + dotProduct);
+                if(dotProduct <= .09f){
                     skip = true;
-                    Utils.print("Culling plane " + dotProduct);
                 }
             }
             if(!skip) {
--- app/src/main/java/com/projects/oleg/viewtotextureconverter/Shader/OES3DShader.java
+++ app/src/main/java/com/projects/oleg/viewtotextureconverter/Shader/OES3DShader.java
@@ -23,6 +23,7 @@ public class OES3DShader extends Shader {
                     "attribute vec4 aPosition;\n" +
                     "attribute vec2 aTextureCoord;\n" +
                     "varying vec2 vTextureCoord;\n" +
+                    "varying vec2 vTextureCoordInv;"+
                     "void main() {\n" +
                     "  vec4 scaledPos = vec4(1,1,1,1); "+
                     "  scaledPos.x = scale.x * aPosition.x;"+
@@ -30,19 +31,22 @@ public class OES3DShader extends Shader {
                     "  scaledPos.z = scale.z * aPosition.z;"+
                     "  gl_Position =  uMVPMatrix * scaledPos;\n " +
                     "  gl_Position =  cameraMatrix * gl_Position; " +
+                    "  vTextureCoordInv = vec2(1.0f / gl_Position.z, 1.0f / gl_Position.z);"+
+                    "  vTextureCoord = vec2(aTextureCoord.x / gl_Position.z, aTextureCoord.y / gl_Position.z);\n" +
                     "  gl_Position.x = (perspective.w * perspective.z * gl_Position.x) / (perspective.w + gl_Position.z);"+
                     "  gl_Position.y = (perspective.w * gl_Position.y) / (perspective.w + gl_Position.z);"+
                     "  gl_Position.z = ( (2.0f*(gl_Position.z - perspective.x))/(perspective.y - perspective.x) ) - 1.0f;"+
-                    "  vTextureCoord = aTextureCoord;\n" +
-                    "}\n";
+                     "}\n";
 
     private final String mFragmentShader =
                     "#extension GL_OES_EGL_image_external : require\n"+
                     "precision mediump float;\n" +
                     "varying vec2 vTextureCoord;\n" +
+                    "varying vec2 vTextureCoordInv;"+
                     "uniform samplerExternalOES sTexture;\n" +
                     "void main() {\n" +
-                    "  gl_FragColor = texture2D(sTexture, vTextureCoord);\n" +
+                    "  vec2 reintTxt = vec2(vTextureCoord.x/vTextureCoordInv.x,vTextureCoord.y/vTextureCoordInv.x);"+
+                    "  gl_FragColor = texture2D(sTexture, reintTxt);\n" +
                     "}\n";
 
 
